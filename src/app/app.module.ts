a1341d664eda54f1faea7795768fe87ca72873f9
LGuarro <luca.guarro@gmail.com>
2017-04-01 15:02:11 -0700

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { SpotifytestComponent } from './spotifytest/spotifytest.component';
+import { LoginComponent } from './login/login.component';
+import { PlaylistManagerComponent } from './playlist-manager/playlist-manager.component';
+import { AuthGuard } from './auth-guard.service';
+import { AuthService } from './auth.service';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'playlist-manager', canActivate: [AuthGuard],component: PlaylistManagerComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SpotifytestComponent
+    LoginComponent,
+    PlaylistManagerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
