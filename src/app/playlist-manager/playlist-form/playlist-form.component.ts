318ef0abc477f5993a980f8b165c3734ef1d7206
LGuarro <luca.guarro@gmail.com>
2017-04-01 21:30:25 -0700

    file name changing and components

--- src/app/playlist-manager/playlist-form/playlist-form.component.ts
+++ src/app/playlist-manager/playlist-form/playlist-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-playlist-form',
@@ -8,6 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class PlaylistFormComponent implements OnInit {
 
   constructor() { }
+
+  @ViewChild('fileContentInput') fileSelectedInput: ElementRef;
+
+  fileSelected: string = "No file selected";
+
+  logToConsole(){
+    console.log(this.fileSelectedInput.nativeElement.value);
+    this.fileSelected = this.fileSelectedInput.nativeElement.value;
+    this.fileSelected = this.getJustFileName(this.fileSelected);
+  }
+
+  getJustFileName(fileWithPath: string){
+    var justThePath = '';
+    for (var i = fileWithPath.length - 1, len = 0; i >= len; i--) {
+      if(fileWithPath[i]==="\\"){
+        return justThePath;
+      } else{
+        justThePath = fileWithPath[i] + justThePath;
+      }
+    }
+    console.log(this.fileSelected);
+    return justThePath;
+  }
 
   ngOnInit() {
   }
